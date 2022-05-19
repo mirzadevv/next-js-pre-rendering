f31cd0da66aa1cbc4e7c9a22313604e095055b7d
mirzaei.dev <mirzaei.devv@gmail.com>
2022-05-20 01:13:08 +0430

    create todos page

---
+++ pages/todos/index.js
@@ -0,0 +1,8 @@
+import styles from "../../styles/todos.module.css";
+export default function Todos() {
+  return (
+    <>
+      <div className={styles.todosContainer}>todos</div>
+    </>
+  );
+}
