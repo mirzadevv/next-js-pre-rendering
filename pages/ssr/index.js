d5c8ec7500d6a4e576f5395703983704d32d2178
mirzaei.dev <mirzaei.devv@gmail.com>
2022-05-20 12:30:05 +0430

    feat: use getServerSideProps for fetching the users in the SSR approach
    this is ServerSideRendering and it means the HTML is generated on each request

--- pages/ssr/index.js
+++ pages/ssr/index.js
@@ -1,8 +1,30 @@
 import styles from "../../styles/ssr.module.css";
-export default function Ssr() {
+export default function Ssr({ users }) {
   return (
     <>
-      <div className={styles.ssrContainer}>SSR</div>
+      <div className={styles.ssrContainer}>
+        {users.map((user) => (
+          <>
+            <h5>
+              <span className={styles.name}> Name: </span> {user.name}
+            </h5>
+            <h5>
+              <span className={styles.email}>Email:</span> {user.email}
+            </h5>
+            <hr />
+          </>
+        ))}
+      </div>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const result = await response.json();
+  return {
+    props: {
+      users: result,
+    },
+  };
 }
