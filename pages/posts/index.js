0e11f666b226810275a91bbbb307d748fe2b6208
mirzaei.dev <mirzaei.devv@gmail.com>
2022-05-20 13:58:13 +0430

    create posts page

---
+++ pages/posts/index.js
@@ -0,0 +1,8 @@
+import styles from "../../styles/posts.module.css";
+export default function Posts() {
+  return (
+    <>
+      <div className={styles.postsContainer}>POSTS SSG </div>
+    </>
+  );
+}
