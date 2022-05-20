3a32b20a03b4571f36baa042e20610e5abdeb451
mirzaei.dev <mirzaei.devv@gmail.com>
2022-05-20 14:13:22 +0430

    feat: create posts page with SSG pre-rendering
    SSG is the pre-rendering method that generates the HTML at build time. The pre-rendered HTML is then reused on each request.

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,31 @@
 import styles from "../../styles/posts.module.css";
-export default function Posts() {
+import Link from "next/link";
+export default function Posts({ posts }) {
+  console.log(posts);
   return (
     <>
-      <div className={styles.postsContainer}>POSTS SSG </div>
+      <div className={styles.postsContainer}>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <Link href={`posts/${post.id}`}>
+              <h5>
+                <span>Title</span> {post.title}
+              </h5>
+            </Link>
+            <hr />
+          </div>
+        ))}
+      </div>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const result = await response.json();
+  return {
+    props: {
+      posts: result,
+    },
+  };
 }
