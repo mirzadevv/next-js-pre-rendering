d7aae2d9342bb4aba3b812ededd42de1ff2644c0
mirzaei.dev <mirzaei.devv@gmail.com>
2022-06-02 14:41:29 +0430

    create todos page into another one

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,55 @@
-import Head from "next/head";
-import styles from "../styles/index.module.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import styles from "../styles/todos.module.css";
 
-export default function Home() {
+export default function Todos() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: uuidv4(),
+        title: inputValue,
+      },
+    ]);
+    setInputValue("");
+  };
+
+  const handleDelete = (todoId) => {
+    const todosClone = [...todos];
+    const todoIndex = todosClone.findIndex((item) => item.id === todoId);
+    todosClone.splice(todoIndex, 1);
+    setTodos(todosClone);
+  };
+
   return (
-    <div className={styles.indexContainer}>
-      <Head>
-        <title>Next Js </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <h1>Index Page</h1>
-    </div>
+    <>
+      <div className={styles.todosContainer}>
+        <input
+          className={styles.input}
+          onChange={(e) => setInputValue(e.currentTarget.value)}
+          value={inputValue}
+        />
+        <button className={styles.button} onClick={handleAddTodo}>
+          Add Todo
+        </button>
+        <div className={styles.todosList}>
+          {todos.length === 0 && <h3>THERE ARE NO TODOS</h3>}
+          {todos.map((todo) => (
+            <div className={styles.todo} key={todo.id}>
+              <p>{todo.title}</p>
+              <button
+                className={styles.deleteBtn}
+                onClick={() => handleDelete(todo.id)}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
